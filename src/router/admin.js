ac4363f9ef9ae2320af52c127077acd03c44e586
George Boakye <georgekwamekumiboakye@gmail.com>
2022-08-15 08:59:41 +0000

    create admin routes

---
+++ src/router/admin.js
@@ -0,0 +1,52 @@
+import AdminDashboardView from "@/views/admin_views/AdminDashboardView.vue"
+import AdminLoginView from "@/views/admin_views/AdminLoginView.vue"
+import ApplicationEntriesView from "@/views/admin_views/ApplicationEntriesView.vue"
+import AssessmentHistoryView from "@/views/admin_views/AssessmentHistoryView.vue"
+import ComposeAssessmentView from "@/views/admin_views/ComposeAssessmentView.vue"
+import CreateApplicationView from "@/views/admin_views/CreateApplicationView.vue"
+import ResultsView from "@/views/admin_views/ResultsView.vue"
+import SettingsView from "@/views/admin_views/SettingsView.vue"
+
+
+export default [
+    {
+        path:'/admin-login',
+        name:'admin-login',
+        component: AdminLoginView
+    },
+    {
+        path:'/admin-dashboard',
+        name:'admin-dashboard',
+        component: AdminDashboardView
+    },
+    {
+        path:'/create-application',
+        name:'create-application',
+        component: CreateApplicationView
+    },
+    {
+        path:'/application-entries',
+        name:'application-entries',
+        component: ApplicationEntriesView
+    },
+    {
+        path:'/compose-assessment',
+        name:'compose-assessment',
+        component: ComposeAssessmentView
+    },
+    {
+        path:'/assessment-history',
+        name:'assessment-history',
+        component: AssessmentHistoryView
+    },
+    {
+        path:'/results',
+        name:'results',
+        component: ResultsView
+    },
+    {
+        path:'/settings',
+        name:'settings',
+        component: SettingsView
+    }
+]
---
+++ src/router/user.js
@@ -0,0 +1,52 @@
+import SignUpView from "@/views/user_views/SignUpView.vue";
+import LoginView from "@/views/user_views/LoginView.vue";
+import PreDashboardView from "@/views/user_views/PreDashboardView.vue";
+import ForgetPasswordView from "@/views/user_views/ForgetPasswordView.vue";
+import DashboardView from "@/views/user_views/DashboardView.vue";
+import AssessmentView from "@/views/user_views/AssessmentView.vue";
+import QuestionsView from "@/views/user_views/QuestionsView.vue";
+import SuccessfulView from "@/views/user_views/SuccessfulView.vue";
+
+export default[
+    {
+        path: "/signup",
+        name: "sign-up",
+        component: SignUpView,
+      },
+      {
+        path: "/login",
+        name: "login",
+        component: LoginView,
+      },
+      {
+        path: "/forget-password",
+        name: "forget-password",
+        component: ForgetPasswordView,
+      },
+      {
+        path: "/pre-dashboard",
+        name: "pre-dashboard",
+        component: PreDashboardView,
+      },
+      {
+        path: "/dashboard",
+        name: "dashboard",
+        component: DashboardView,
+      },
+    
+      {
+        path: "/assessment",
+        name: "assessment",
+        component: AssessmentView,
+      },
+      {
+        path: "/questions",
+        name: "questions",
+        component: QuestionsView,
+      },
+      {
+        path: "/successful",
+        name: "successful",
+        component: SuccessfulView,
+      },
+    ]
